fb1546d962f2085ff8b305965146037e193ae740
배하은 <haeun@baeha-eun-ui-MacBookPro.local>
2024-04-06 10:52:57 +0900

    Your commit message

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,18 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "A Blank Canvas",
   description: "Haeun's website",
+  icons: {
+    icon:[
+      '/favicon/favicon.ico?v=1',
+    ],
+    apple:[
+      '/favicon/apple-touch-icon.png?v=4',
+    ],
+    shortcut:[
+      '/favicon/apple-touch-icon.png',
+    ],
+  },
+  manifest: '/site.webmanifast'
 };
 
 export default function RootLayout({
@@ -18,6 +30,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="stylesheet" href="https://use.typekit.net/qko1ckz.css" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body >{children}</body>
     </html>
