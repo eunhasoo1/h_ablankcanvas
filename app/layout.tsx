580706c6f0e86f1534eb76fe7e362b4d0343e17d
배하은 <haeun@baeha-eun-ui-MacBookPro.local>
2024-04-07 09:56:12 +0900

    Your commit message

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,6 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="stylesheet" href="https://use.typekit.net/qko1ckz.css" />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body >{children}</body>
     </html>
