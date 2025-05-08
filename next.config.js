10fa58620f7b5230da318bb1790ddbee1b5aea3e
eunhasoo1 <bhekkh1@gmail.com>
2025-05-08 17:02:06 +0900

    Update next.config.js to use static export and include public files

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ['img.youtube.com'],
   },
+  output: 'export',
+  // 이 설정은 public 폴더의 파일들을 out 디렉토리로 복사하게 합니다
+  distDir: 'out',
 };
 
 module.exports = nextConfig; 
